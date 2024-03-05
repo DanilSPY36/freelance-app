24e7dd2efc0524803f429f97e6573dce3e0035ea
Danil Nazarko <danilspy36@gmail.com>
2024-03-05 19:30:27 +0300

    install tailwindcss

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import personSlice from "./slices/personSlice";
+import orderSlice from "./slices/orderSlice";
  
 export const store = configureStore({
     reducer: {
         personState: personSlice,
+        orderSlice: orderSlice
     },
 });
